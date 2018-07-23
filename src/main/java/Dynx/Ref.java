3b5e085ca9c74319a9bee8232d486aa622876b72
PhilHarrington <40513186+PhilHarrington@users.noreply.github.com>
2018-07-23 20:27:53 +0300

    Add files via upload

---
+++ src/main/java/Dynx/Ref.java
@@ -0,0 +1,6 @@
+package Dynx;
+
+public class Ref {
+                // CHANGE THE TOKEN FIELD TO THE TOKEN CREATED THROUGH THE BOT USER MADE ON THE DISCORD DEVELOPERS PAGE
+    public static final String token = "NDcwNjgyNTYwMDAzODMzODY3.DjeV6Q.vhsjhSkc4D_6pMdPwX1pWi8X6f0";
+}
